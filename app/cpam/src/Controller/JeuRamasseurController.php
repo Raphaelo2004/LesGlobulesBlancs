494a7bf80ec27de7e20f36332fb882f2d311f987
Raphaelo2004 <fabyangalaxy@gmail.com>
2025-03-20 19:52:35 +0100

    debut jeu 2

--- app/cpam/src/Controller/JeuRamasseurController.php
+++ app/cpam/src/Controller/JeuRamasseurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\JeuRamasseurService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,20 @@ class JeuRamasseurController extends AbstractController
     /**
      * @Route("/jeu/ramasseur", name="app_jeu_ramasseur")
      */
-    public function index(): Response
+    public function index(JeuRamasseurService $jeuRamasseurService): Response
     {
         return $this->render('jeu_ramasseur/index.html.twig', [
             'controller_name' => 'JeuRamasseurController',
+            'gameItems' => $jeuRamasseurService->getJeuRamasseurItems(),
+            'activeGame' => 'presentation' // Indique la page active
         ]);
+    }
+
+    /**
+     * @Route("/jeu/ramasseur_gameplay", name="app_jeu_ramasseur_gameplay")
+     */
+    public function gameplay(): Response
+    {
+        return $this->render('jeu_ramasseur/gameplay.html.twig');
     }
 }
