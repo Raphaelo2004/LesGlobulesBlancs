c9306e7ab904ed2bc699990873f3bf5b6cfd9220
Dorian <dorian.police@etudiant.univ-rennes1.fr>
2025-03-27 11:05:14 +0100

    Maj Routes Panneau liege + Presentation Jeu Ninja + Controller

---
+++ app/cpam/src/Controller/JeuNinjaController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\JeuNinjaService;
+use App\Service\ClassementService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JeuNinjaController extends AbstractController
+{
+    private $classementService;
+    public function __construct(ClassementService $classementService)
+    {
+        $this->classementService = $classementService;
+    }
+
+    /**
+     * @Route("/jeu/ninja", name="app_jeu_ninja")
+     */
+    public function index(JeuNinjaService $jeuNinjaService): Response
+    {
+        // Récupérer le classement
+        $classement = $this->classementService->getClassement();
+
+        return $this->render('jeu_ninja/index.html.twig', [
+            'controller_name' => 'JeuNinjaController',
+            'gameItems' => $jeuNinjaService->getJeuNinjaItems(),
+            'activeGame' => 'presentation', // Indique la page active
+            'classement' => $classement, 
+        ]);
+    }
+
+    /**
+     * @Route("/jeu/ninja_gameplay", name="app_jeu_ninja_gameplay")
+     */
+    public function gameplay(): Response
+    {
+        $classement = $this->classementService->getClassement();
+
+        return $this->render('jeu_ninja/gameplay.html.twig', [
+            'classement' => $classement
+        ]);
+    }
+}
